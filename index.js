2edd1500c071aab9b04f28a7fed23ebadeb182f6
Petra Söderström <101672601+Petrasoderstrom1612@users.noreply.github.com>
2024-10-28 09:37:44 +0100

    continue and break

--- index.js
+++ index.js
@@ -109,3 +109,33 @@ const playlistHtml2 = []
 
 document.getElementById('container2').innerHTML = playlistHtml2.join("")
 /*-------------------------------------------------------------------------------------------------------------*/
+
+/* CONTINUE and BREAK - continue immediatelly stops a loop and goes onto next itteration, break stops all remaining itterations and ends the forloop*/ 
+
+const expensesAndRefunds = [
+    { description: 'Groceries', amount: 50, year: 2023 },
+    { description: 'Electronics', amount: -30, year: 2023 },
+    { description: 'Dinner', amount: 40, year: 2023 },
+    { description: 'Clothing', amount: 60, year: 2023 },
+    { description: 'Entertainment', amount: 25, year: 2023 },
+    { description: 'Rent', amount: -500, year: 2024 },
+    { description: 'Utilities', amount: 100, year: 2024 },
+    { description: 'Books', amount: 20, year: 2024 },
+    { description: 'Fitness', amount: 30, year: 2024 },
+    { description: 'Gifts', amount: 15, year: 2024 },
+]
+
+let totalSpent = 0
+const cutoffDate = 2024
+
+for (let i= 0; i < expensesAndRefunds.length; i++){
+    if(expensesAndRefunds[i].amount < 0){
+        continue
+    }
+    if (cutoffDate === expensesAndRefunds[i].year){
+        break
+    }
+    totalSpent += expensesAndRefunds[i].amount //the forloop will check both if statements on each itteration before adding the amount so what you see is one forloop
+}
+
+console.log(`The total expenses spent in 2023 without credits is $${totalSpent}`)
